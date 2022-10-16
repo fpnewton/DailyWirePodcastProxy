5754ff9bff89022ebaeaf48f509b46a0c02da604
Fraser Newton <fpnewton90@gmail.com>
2022-10-16 04:27:14 -0400

    Updated PKGBUILD

--- build/build.sh
+++ build/build.sh
@@ -13,5 +13,16 @@ rm -rf ./publish
 
 cp ./build/dailywire-podcast-proxy.service ./publish/linux-x64/dailywire-podcast-proxy.service
 
+pushd ./publish
+zip -r linux-x64.zip ./linux-x64
+zip -r win-x64.zip ./win-x64
+zip -r osx-x64.zip ./osx-x64
+popd
+
+cd ./build/
+makepkg --clean
+mv ./dailywire-podcast-proxy-*.pkg.tar.zst ../publish
+rm -rf DailyWirePodcastProxy
+
 popd
 popd
