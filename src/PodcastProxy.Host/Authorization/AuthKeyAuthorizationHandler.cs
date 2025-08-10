db5986592fb8557dc3a13bf84582dcf5c55d4983
Fraser Newton <fpnewton@users.noreply.github.com>
2025-08-10 00:47:41 -0400

    Removed noisy token auth success logging.

--- src/PodcastProxy.Host/Authorization/AuthKeyAuthorizationHandler.cs
+++ src/PodcastProxy.Host/Authorization/AuthKeyAuthorizationHandler.cs
@@ -44,7 +44,6 @@ public class AuthKeyAuthorizationHandler(
             if (keyMatches)
             {
                 context.Succeed(requirement);
-                logger.LogInformation("Authorization succeeded");
             }
             else
             {
